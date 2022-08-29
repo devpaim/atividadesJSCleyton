f6ce14b31b145c7558cbb9d0587e4cc761285c69
Mateus Paim <mateuspaiim@gmail.com>
2022-08-29 11:47:49 -0300

    First exercise

---
+++ AtividadeJS/Atividade.js
@@ -0,0 +1,14 @@
+alert('Vamos fazer a primeira atividade')
+let valorNumeroUm = 10
+let valorNumeroDois = 5
+let resultado = 0
+const mensagem = ' Resultado da atividade é '
+
+resultado = parseInt(valorNumeroUm) + parseInt(valorNumeroDois)
+console.log(mensagem + resultado)
+
+alert('vamos fazer a segunda atividade')
+numeroUm = prompt('Digite o primeiro número')
+numeroDois = prompt('Digite o segundo numero')
+console.log(mensagem)
+console.log(parseInt(numeroUm) + parseInt(numeroDois))

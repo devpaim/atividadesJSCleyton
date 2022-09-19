7ec4bc56950d2e91dd9fb7993bb2655794364fc9
Mateus Paim <mateuspaiim@gmail.com>
2022-09-19 11:49:10 -0300

    Atividade Objetos

---
+++ AtividadeJS/objetos.js
@@ -0,0 +1,32 @@
+let apartamento = {
+  banheiros: 2,
+  quartos: 2,
+  suites: 2,
+  Cozinha: 1,
+  AreaLazer: '20m²',
+  Garagem: '2 Carros',
+
+  corParedeBanheiros: 'azul',
+  corParedeQuartos: 'Cinza',
+
+  Cozinha: {
+    MesaMamore: '20 metros',
+    Geladeira: {
+      marca: 'Brastemp',
+      frezzer: '5 litros'
+    },
+    fogao: {
+      marca: 'JovemPan',
+      bocas: 6,
+      forno: '12 litros'
+    },
+    ParedeCozinha: 'AzulejosAzuis'
+  },
+  salaDeEstar: {
+    televisão: '29, Samsung',
+    sofa: 'SofaCama, Azul',
+    Frigobar: '10 litros'
+  }
+}
+
+console.log(apartamento)
